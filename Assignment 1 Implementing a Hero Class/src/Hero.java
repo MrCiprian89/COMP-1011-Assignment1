51ef66b74f0a23f3df7525da65b9c00ae3b2828d
MrCiprian89 <mr.ciprian89@gmail.com>
2015-01-23 00:21:15 -0500

    Created methods and properties in Hero Class

--- Assignment 1 Implementing a Hero Class/src/Hero.java
+++ Assignment 1 Implementing a Hero Class/src/Hero.java
@@ -1,4 +1,41 @@
 
 public class Hero {
-
-}
+//PRIVATE VARIABLES ##################################################################
+	private int strength,speed,health;
+	public String name;
+	
+//CONSTRUCTOR
+	public Hero(String name){
+		this.name = name;
+		generateAbilities();
+	}
+//PRIVATE METHODS
+	private void generateAbilities(){
+		this.strength = (int) (Math.random()*100+1);
+		this.speed = (int) (Math.random()*100+1);
+		this.health = (int) (Math.random()*100+1);
+	}
+	private boolean hitAttempt(){
+		int hitChance = 2;
+		int checkHitSuccess = (int) (Math.random()*10 +1);
+		if (checkHitSuccess > hitChance){
+			return false;
+		}
+			else{return true;}
+	}
+	private int hitDamage(){
+		int dmgBonus = (int)(Math.random()*6 +1);
+		int dmgDealt = this.strength + dmgBonus;
+		return dmgDealt;
+	}
+	
+//PUBLIC METHODS
+	public void fight(){
+		if(hitAttempt()){
+			System.out.println(hitDamage());
+		}
+	}
+	public void show(){
+		System.out.println(this.name +"'s abilites are \n Strength: " + this.strength + "\n Speed: " +this.speed + "\n Health: " + this.health);
+	}
+}//END class
