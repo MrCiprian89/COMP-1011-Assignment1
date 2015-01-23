1e01f5e545b98ccebaf14e98ccf38aa7c20f4360
MrCiprian89 <mr.ciprian89@gmail.com>
2015-01-22 23:56:29 -0500

    Created Project Structure

---
+++ Assignment 1 Implementing a Hero Class/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Steve
+ * Class Description: This Project creates a Hero class that will be instantiated and tested
+ * @version 0.1 Class structures created
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
